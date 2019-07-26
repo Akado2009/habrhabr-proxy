0b3d7d4b6506e975f06fad15e09f6849c819f77b
Akado2009 <kirillprosvirov@Kirills-MacBook-Pro.local>
2019-07-26 06:59:43 +0300

    requirements added

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from http.server import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -6,17 +6,20 @@ IP_ADDRESS = "127.0.0.1"
 PORT_NUMBER = 8080
 BASE_URL = "https://habr.com"
 
+
 def check_path(path):
     fake_gets = ['/fonts', '/images', '/site', '/favicon']
-    for fake in fake_gets: 
-        if path.startswith(fake): return False
+    for fake in fake_gets:
+        if path.startswith(fake):
+            return False
     return True
+
 
 def add_trademark(word):
     punctuation = ['.', ',', '?', '!', ':', ';']
     tm_char = '™'
     if word.endswith(tuple(punctuation)):
-        if len(word) == 7: 
+        if len(word) == 7:
             return word[:-1] + tm_char + word[-1]
         return word
     if len(word) == 6:
@@ -27,16 +30,19 @@ def add_trademark(word):
 class CustomHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if check_path(self.path): 
+        if check_path(self.path):
             habr_html = urllib.request.urlopen(BASE_URL + self.path)
             habr_content = habr_html.read()
             soup = BeautifulSoup(habr_content, "html.parser")
             self.send_response(200)
-            self.send_header('Content-type','text/html')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
             for a in soup.findAll('a', href=True):
                 if BASE_URL in a['href']:
-                    a['href'] = a['href'].replace(BASE_URL, 'http://{}:{}'.format(IP_ADDRESS, PORT_NUMBER))
+                    a['href'] = a['href'].replace(
+                        BASE_URL,
+                        'http://{}:{}'.format(IP_ADDRESS, PORT_NUMBER)
+                    )
             for element in soup.body.findAll(text=True):
                 text = element.string.strip()
                 if text:
@@ -44,8 +50,8 @@ class CustomHandler(BaseHTTPRequestHandler):
                     words = list(map(add_trademark, words))
                     element.replace_with(' '.join(words) + ' ')
             self.wfile.write(str(soup).encode())
-        
         return
+
 
 try:
     server = HTTPServer((IP_ADDRESS, PORT_NUMBER), CustomHandler)
@@ -53,7 +59,3 @@ try:
 
 except KeyboardInterrupt:
     server.socket.close()
-
-    # check pep8
-    # check urls for redirect
-    
